728ef318d51e7121d46d024930379f5108e03341
seron <seroncj@gmail.com>
2022-08-28 08:28:25 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form =document.querySelector('form');
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const inputText = e.target.querySelector('#new-task-description').value;
+  
+    const ul= e.target.parentNode.querySelector('#tasks');
+    const btn =document.createElement('button');
+    const li =document.createElement('li')
+  
+    btn.innerText ='X'
+    li.innerText = `${inputText} `;
+    li.appendChild( btn);
+    ul.append(li);
+  
+    form.reset();
+    btn.addEventListener('click',(e)=>{
+      e.target.parentNode.remove();
+    });
+  });
+  }); 
